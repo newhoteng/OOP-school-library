523f3b47618993fe1ca8de89de95541a46fd2c86
Harriet Oteng <harriet.oteng@gmail.com>
2023-06-21 15:25:15 +0000

    Create a many-to-many relationship b/n Person and Book through Rental

--- rental.rb
+++ rental.rb
@@ -1,13 +1,14 @@
 class Rental
   attr_accessor :date
+  attr_reader :book, person
 
-  def initialize(date)
+  def initialize(date, book, person)
     @date = date
+
+    @book = book
+    book.rentals << self
+
+    @person = person
+    person.rentals << self
   end
 end
-
-Create the has-many side of Book and Rental (a book has many rentals).
-Create the belongs-to side of Rental and Book (a rental belongs to a book).
-Create the has-many side of Person and Rental (a person has many rentals).
-Create the belongs-to side of Rental and Person (a rental belongs to a person).
-Modify the constructor of Rental so Book and Person are set in it.
--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :classroom 
+  attr_reader :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
@@ -11,6 +11,7 @@ class Student < Person
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
+  end
 
   def play_hooky
     '"¯\(ツ)/¯"'
