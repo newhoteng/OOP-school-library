2a73a52ae6b3b136a3b66285243d6358ada0403c
Seyi Oyemade <seyioyemade918@gmail.com>
2023-06-27 14:32:07 +0100

    Add 'save to database' functions

--- app.rb
+++ app.rb
@@ -140,4 +140,40 @@ class App
   end
 
   # Save to database
+  def save_books
+    updated_books = []
+
+    @books.each do |book|
+      updated_books << { 'title' => book.title, 'author' => book.author }
+    end
+
+    File.write('data/books.json', JSON.pretty_generate(updated_books))
+  end
+
+  def save_people
+    updated_people = []
+
+    @people.each do |person|
+      if person.instance_of?(::Teacher)
+        updated_people << { 'type' => 'Teacher', 'id' => person.id, 'name' => person.name, 'age' => person.age,
+                            'specialization' => person.specialization }
+      elsif person.instance_of?(::Student)
+        updated_people << { 'type' => 'Student', 'id' => person.id, 'name' => person.name, 'age' => person.age,
+                            'parent_permission' => person.parent_permission }
+      end
+    end
+
+    File.write('data/people.json', JSON.pretty_generate(updated_people))
+  end
+
+  def save_rentals
+    updated_rentals = []
+
+    @rentals.each do |rental|
+      updated_rentals << { 'person_name' => rental.person.name, 'book_titles' => rental.book.title,
+                           'date' => rental.date }
+    end
+
+    File.write('data/rentals.json', JSON.pretty_generate(updated_rentals))
+  end
 end
