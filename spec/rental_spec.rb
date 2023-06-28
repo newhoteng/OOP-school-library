503113863fefda5ef8abfc3e9ce4a3965f177209
Harriet Oteng <harriet.oteng@gmail.com>
2023-06-28 16:42:10 +0000

    Add tests for Rental class

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,1 +1,16 @@
+require_relative '../rental'
+require_relative '../student'
+require_relative '../book'
 
+describe Rental do
+  context 'Check Teacher creating tasks' do
+    it 'should create a Teacher' do
+      person = Student.new(15, nil, 'Kyei')
+      book = Book.new('Game of Thrones', 'George Martins')
+      rental = Rental.new('2023/06/28', book, person)
+      expect(rental.person.name).to eq('Kyei')
+      expect(rental.book.author).to eq('George Martins')
+      expect(person.rentals[0].book.title).to eq('Game of Thrones')
+    end
+  end
+end
